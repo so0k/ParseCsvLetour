66a7217ff1985fff303b9bd0b4b883d1450062cc
Vincent De Smet <vincent.drl@gmail.com>
2015-08-11 23:35:02 +0700

    git repo init

---
+++ Parse.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-FileName {   
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "Comma Separated files (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+$Input  = Get-FileName
+$Output = ".\Output.csv"
+
+if (Test-Path $Output) {
+    echo "$Output already exists and will be overwritten (Ctlr-C to abort)"
+    pause
+}
+
+echo "Testing if nuget is available"
+Try {
+    gcm nuget 2> $null
+    $nuget = nuget
+} Catch {
+    #we'll use local nuget instead
+    $nuget = ".\nuget.exe"
+    if (!(Test-Path $nuget))
+    {
+        #download nuget
+        echo "Downloading nuget to this directory"
+        iwr https://www.nuget.org/nuget.exe -OutFile $nuget
+    }
+}
+echo "Installing HtmlAgilityPack version 1.4.9"
+& $nuget install HtmlAgilityPack -Version 1.4.9
+add-type -Path "HtmlAgilityPack.1.4.9\lib\Net40\HtmlAgilityPack.dll"
+$doc = New-Object HtmlAgilityPack.HtmlDocument 
+
+echo "Parsing $Input"
+ConvertFrom-Csv (gc $Input)| % {
+    
+    $doc.LoadHTML($_.Description) > $null
+    [PSCustomObject]@{ 
+        "SKU" = $_."SKU*";
+        "Description" = $doc.DocumentNode.SelectNodes("//div[@class='desc']").OuterHtml -Join " "
+    }
+} | Export-Csv $Output -NoTypeInformation
+echo "Done"
---
+++ README.md
@@ -0,0 +1,23 @@
+## Introduction
+
+This is a proof of concept powershell script to parse HTML contained within CSV file.
+
+## Quickstart
+
+To run from powershell command line:
+
+```powershell
+iwr https://goo.gl/N3VnMj | iex
+```
+
+## Demo
+
+![screencast.gif](screencast.gif "screencast")
+
+ref: https://www.reddit.com/r/excel/comments/3gfae8/extracting_specific_html_code_from_within_a_cell/
+
+## License
+
+This code is released under the GNU General Public License (GPL).
+
+The GPL is a copyleft license that requires anyone who distributes this code or a derivative work to make the source available under the same terms.
