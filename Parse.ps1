8e4dc057aaffdaeac27130acd31a7644b170a6ab
Vincent De Smet <vincent.drl@gmail.com>
2015-08-12 00:25:46 +0700

    fixing invalid characters in Parse.ps1

--- Parse.ps1
+++ Parse.ps1
@@ -1,4 +1,8 @@
-﻿Function Get-FileName {   
+﻿#Script to parse csv file
+
+$OutputFile = ".\Output.csv"
+
+Function Get-FileName {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDialog.initialDirectory = $initialDirectory
@@ -7,40 +11,44 @@
     $OpenFileDialog.filename
 }
 
-$Input  = Get-FileName
-$Output = ".\Output.csv"
+Function Test-Command( [string] $CommandName )
+{
+    (Get-Command $CommandName -ErrorAction SilentlyContinue -WarningAction SilentlyContinue) -ne $null
+}
+$InputFile  = Get-FileName
 
-if (Test-Path $Output) {
-    echo "$Output already exists and will be overwritten (Ctlr-C to abort)"
+if (Test-Path $OutputFile) {
+    echo "$OutputFile already exists and will be overwritten (Ctlr-C to abort)"
     pause
 }
 
 echo "Testing if nuget is available"
-Try {
-    gcm nuget 2> $null
-    $nuget = nuget
-} Catch {
-    #we'll use local nuget instead
-    $nuget = ".\nuget.exe"
-    if (!(Test-Path $nuget))
-    {
-        #download nuget
-        echo "Downloading nuget to this directory"
+if (!(Test-Path ".\nuget.exe"))
+{
+    #it's not local, is it on path?
+    $nuget = "nuget"
+    if (!(Test-Command("nuget"))) {
+        $nuget = ".\nuget.exe"
+        echo ">>Downloading nuget to this directory"
         iwr https://www.nuget.org/nuget.exe -OutFile $nuget
+    } else {
+        echo ">>nuget was found globally"
     }
+} else {
+    echo ">>nuget was found locally"
 }
 echo "Installing HtmlAgilityPack version 1.4.9"
 & $nuget install HtmlAgilityPack -Version 1.4.9
 add-type -Path "HtmlAgilityPack.1.4.9\lib\Net40\HtmlAgilityPack.dll"
 $doc = New-Object HtmlAgilityPack.HtmlDocument 
 
-echo "Parsing $Input"
-ConvertFrom-Csv (gc $Input)| % {
+echo "Parsing $InputFile"
+ConvertFrom-Csv (gc $InputFile)| % {
     
     $doc.LoadHTML($_.Description) > $null
     [PSCustomObject]@{ 
         "SKU" = $_."SKU*";
         "Description" = $doc.DocumentNode.SelectNodes("//div[@class='desc']").OuterHtml -Join " "
     }
-} | Export-Csv $Output -NoTypeInformation
+} | Export-Csv $OutputFile -NoTypeInformation
 echo "Done"
